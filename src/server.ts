7dfd8b84561f2362e4f067c521b463442bef7f07
breno-aredes <breno_mg10@hotmail.com>
2023-06-07 23:38:01 -0300

    chore: Remove unused libraries and replace nodemon with tsx

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import app, { init } from "./app";
 
-const port = +process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 init().then(() => {
   app.listen(port, () => {
-    /* eslint-disable-next-line no-console */
     console.log(`Server is listening on port ${port}.`);
   });
 });
