d7ede9a40bbf62c6e8e50f42a73bddd4e55b4810
breno-aredes <breno_mg10@hotmail.com>
2024-01-11 12:58:54 -0300

    feat: now the training descrition is optional

---
+++ prisma/migrations/20240111155428_optional_descripton/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "training" ALTER COLUMN "description" DROP NOT NULL;
--- src/schemas/trainingSchema.ts
+++ src/schemas/trainingSchema.ts
@@ -10,6 +10,6 @@ const exerciseSchema = Joi.object({
 export const createTrainingSchema = Joi.object<Training>({
   name: Joi.string().required(),
   type: Joi.string().valid("circuit", "series").required(),
-  description: Joi.string().required(),
+  description: Joi.string().allow(""),
   exercises: Joi.array().items(exerciseSchema).min(1).required(),
 });
